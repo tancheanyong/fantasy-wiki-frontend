4662c1e108ada53d079609dd0ece0c31e51cb2a7
tancheanyong <tancheanyong@gmail.com>
2022-05-07 18:33:43 +0800

    feat: Handle form submission to backend

--- src/components/CharacterCreation.tsx
+++ src/components/CharacterCreation.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CharacterCreation() {
-  return <div>CharacterCreation</div>;
+  const [input, setInput] = useState('');
+
+  const handleChange = (e: any) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    fetch('http://localhost:8000/characters/character-creation', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ name: input }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log('result: ', result);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Character Creation</h2>
+      <form action="" method="POST" onSubmit={handleSubmit}>
+        <input type="text" name="name" onChange={handleChange} value={input} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default CharacterCreation;
