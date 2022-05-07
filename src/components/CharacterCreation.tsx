aa9c2d91174402eabe38fc81e05c7b842e834456
tancheanyong <tancheanyong@gmail.com>
2022-05-07 19:39:50 +0800

    feat: Add input fields for character name, age, and description

--- src/components/CharacterCreation.tsx
+++ src/components/CharacterCreation.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 
 function CharacterCreation() {
-  const [input, setInput] = useState('');
-
-  const handleChange = (e: any) => {
-    setInput(e.target.value);
-  };
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [desc, setDesc] = useState('');
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -14,7 +12,7 @@ function CharacterCreation() {
       headers: {
         'Content-type': 'application/json',
       },
-      body: JSON.stringify({ name: input }),
+      body: JSON.stringify({ name, age, desc }),
     })
       .then((response) => response.json())
       .then((result) => {
@@ -26,8 +24,35 @@ function CharacterCreation() {
     <div>
       <h2>Character Creation</h2>
       <form action="" method="POST" onSubmit={handleSubmit}>
-        <input type="text" name="name" onChange={handleChange} value={input} />
-        <button type="submit">Submit</button>
+        <div className="input-field">
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </div>
+        <div className="input-field">
+          <label htmlFor="age">Age: </label>
+          <input
+            type="text"
+            name="age"
+            onChange={(e) => setAge(e.target.value)}
+            value={age}
+          />
+        </div>
+        <div className="input-field">
+          <label htmlFor="desc">Description: </label>
+          <textarea
+            name="desc"
+            onChange={(e) => setDesc(e.target.value)}
+            value={desc}
+          ></textarea>
+        </div>
+        <button className="submit-btn" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
