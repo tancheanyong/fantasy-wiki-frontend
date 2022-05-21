ec917180289ae75f409577df2212716cb25ce4dc
tancheanyong <tancheanyong@gmail.com>
2022-05-21 16:10:53 +0800

    feat: Add BlueprintJS css

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import 'normalize.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
