88954fe38ebea94fb1d810474edbfa1c8cbb9fb9
tancheanyong <tancheanyong@gmail.com>
2022-06-04 11:29:18 +0800

    feat: Character section UI

--- src/components/storyBoard/CharactersPanel/CharactersPanel.tsx
+++ src/components/storyBoard/CharactersPanel/CharactersPanel.tsx
@@ -1,7 +1,25 @@
+import CharacterSectionItem from './CharacterSectionItem';
 import './CharactersPanel.scss';
 
 const CharactersPanel = () => {
-  return <div className="characters-panel">CharactersPanel</div>;
-};
+  const tempCharacters = [
+    { name: 'Johnny', id: '1' },
+    { name: 'Ba', id: '2' },
+    { name: 'Fury', id: '3' },
+    { name: 'Anastasia', id: '4' },
+    { name: 'Anya', id: '5' },
+  ];
+  // TODO: Trim off long character names
 
+  return (
+    <div className="characters-panel">
+      <p className="side-panel__header">Characters</p>
+      <div className="characters-section__container">
+        {tempCharacters.map((character) => (
+          <CharacterSectionItem character={character} key={character.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default CharactersPanel;
