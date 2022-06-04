77d4148da50f4e2a8d2c895c73bc42521de076cf
tancheanyong <tancheanyong@gmail.com>
2022-06-04 09:14:17 +0800

    feat: Event panel UI

--- src/components/storyBoard/EventsPanel/EventCard.tsx
+++ src/components/storyBoard/EventsPanel/EventCard.tsx
@@ -1,10 +1,27 @@
-import { Card } from '@blueprintjs/core';
+import { Card, Icon, IconSize } from '@blueprintjs/core';
 import './EventCard.scss';
 
 const EventCard = () => {
   return (
     <Card className="event-card">
-      <p>Event 1</p>
+      <p className="title">Event 1</p>
+      <div className="character-roster">
+        <Icon
+          icon={'person'}
+          size={IconSize.LARGE}
+          className="character-section__item-icon"
+        />
+        <Icon
+          icon={'person'}
+          size={IconSize.LARGE}
+          className="character-section__item-icon"
+        />
+        <Icon
+          icon={'person'}
+          size={IconSize.LARGE}
+          className="character-section__item-icon"
+        />
+      </div>
     </Card>
   );
 };
--- src/components/storyBoard/EventsPanel/EventsPanel.tsx
+++ src/components/storyBoard/EventsPanel/EventsPanel.tsx
@@ -1,13 +1,18 @@
-import { Card } from '@blueprintjs/core';
+import { Button, Card, Icon, IconSize } from '@blueprintjs/core';
 import EventCard from './EventCard';
 import './EventsPanel.scss';
 
 const EventsPanel = () => {
   return (
     <div className="events-panel">
-      <EventCard />
-      <EventCard />
-      <EventCard />
+      <div className="cards-roster">
+        <EventCard />
+        <EventCard />
+        <EventCard />
+      </div>
+      <button className="add-card-btn" type="button">
+        <span className="bp4-icon-standard bp4-icon-add"></span>
+      </button>
     </div>
   );
 };
