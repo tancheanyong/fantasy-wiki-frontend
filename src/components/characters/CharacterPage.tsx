ed9e34d6132bc4627eace8281ab841e37408a15e
tancheanyong <tancheanyong@gmail.com>
2022-06-05 17:39:54 +0800

    refactor: Move types file into src folder

---
+++ src/components/characters/CharacterPage.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { characterInfo, deleteObject } from '../../types';
+
+function CharacterPage() {
+  const params = useParams();
+  const { name } = params;
+  const [characterInfo, setCharacterInfo] = useState<characterInfo>();
+  const [deleteStatus, setDeleteStatus] = useState<string>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/characters/${name}`)
+      .then((res) => res.json())
+      .then((data: characterInfo) => setCharacterInfo(data));
+  }, []);
+
+  const handleNavigation = () => {
+    setTimeout(() => {
+      navigate('/characters');
+    }, 2000);
+  };
+
+  const deleteCharacter = () => {
+    fetch(`http://localhost:8000/characters/${name}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ _id: characterInfo?._id }),
+    })
+      .then((res) => res.json())
+      .then((result: deleteObject) => {
+        if (result.deletedCount) {
+          setDeleteStatus(`${characterInfo?.name} is successfully deleted.`);
+        } else {
+          setDeleteStatus(`${characterInfo?.name} has already been deleted`);
+        }
+      })
+      .then(() => handleNavigation());
+  };
+
+  return (
+    <div>
+      <h2>CharacterPage</h2>
+      <div>Name: {characterInfo?.name}</div>
+      <div>age: {characterInfo?.age.toString()}</div>
+      <div>Description: {characterInfo?.description}</div>
+      <div>
+        <button onClick={deleteCharacter}>Delete this character</button>
+        <button>Edit this character</button>
+      </div>
+      <p>{deleteStatus}</p>
+    </div>
+  );
+}
+
+export default CharacterPage;
--- src/components/storyBoard/StoryBoard.tsx
+++ src/components/storyBoard/StoryBoard.tsx
@@ -2,7 +2,6 @@ import { Card, Elevation, Button } from '@blueprintjs/core';
 import StoryBoardCanvas from './StoryBoardCanvas/StoryBoardCanvas';
 import './StoryBoard.scss';
 import EventsPanel from './EventsPanel/EventsPanel';
-import CharactersPanel from './CharactersPanel/CharactersPanel';
 import EditNodePanel from './EditNodePanel/EditNodePanel';
 
 function StoryBoard() {
