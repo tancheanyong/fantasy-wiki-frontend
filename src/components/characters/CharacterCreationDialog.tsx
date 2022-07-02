0e0b7241e403069d2feae26d06183572bc1d1156
tancheanyong <tancheanyong@gmail.com>
2022-07-02 22:33:36 +0800

    feat: Post new character to backend

--- src/components/characters/CharacterCreationDialog.tsx
+++ src/components/characters/CharacterCreationDialog.tsx
@@ -1,5 +1,5 @@
-import { Dialog, InputGroup } from '@blueprintjs/core';
-import React, { FunctionComponent } from 'react';
+import { Button, Dialog, InputGroup } from '@blueprintjs/core';
+import React, { FormEvent, FunctionComponent, useState } from 'react';
 
 type CharacterCreationDialogProps = {
   dialogOpen: boolean;
@@ -9,11 +9,50 @@ type CharacterCreationDialogProps = {
 const CharacterCreationDialog: FunctionComponent<
   CharacterCreationDialogProps
 > = ({ dialogOpen, handleDialogOpenClose }) => {
+  const [characterInfo, setCharacterInfo] = useState({ name: '', desc: '' });
+
+  const handleSubmit = () => {
+    console.log(characterInfo);
+    fetch('http://localhost:8000/characters/character-creation', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: characterInfo.name,
+        age: '',
+        description: characterInfo.desc,
+      }),
+    });
+  };
+
   return (
     <Dialog isOpen={dialogOpen} onClose={() => handleDialogOpenClose(false)}>
+      {/* TODO: Handle picture */}
       <div className="picture-upload">Upload Picture</div>
-      <InputGroup placeholder="Name" />
-      <InputGroup placeholder="Description" />
+      <InputGroup
+        placeholder="Name"
+        onBlur={(e) =>
+          setCharacterInfo((prev) => {
+            prev.name = e.target.value;
+            return { ...prev };
+          })
+        }
+      />
+      <InputGroup
+        placeholder="Description"
+        onBlur={(e) =>
+          setCharacterInfo((prev) => {
+            prev.desc = e.target.value;
+            return { ...prev };
+          })
+        }
+      />
+      <Button
+        text="Save Character"
+        type="button"
+        onClick={() => handleSubmit()}
+      />
     </Dialog>
   );
 };
