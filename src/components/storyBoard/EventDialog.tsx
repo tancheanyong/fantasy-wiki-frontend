0c59114cbd9d0f594661533f236f5cd8d6fea4b3
tancheanyong <tancheanyong@gmail.com>
2022-08-14 21:20:33 +0800

    feat: Event dialog UI

--- src/components/storyBoard/EventDialog.tsx
+++ src/components/storyBoard/EventDialog.tsx
@@ -1,10 +1,44 @@
-import { Dialog } from '@blueprintjs/core';
+import { Button, Dialog, InputGroup, Label, TextArea } from '@blueprintjs/core';
 import React, { useContext } from 'react';
 import { StoryBoardContext } from './StoryBoard';
+import './EventDialog.scss';
 
 const EventDialog = () => {
-  const { eventDialogOpen } = useContext(StoryBoardContext);
-  return <Dialog isOpen={eventDialogOpen}>Hi</Dialog>;
+  const { eventDialogOpen, openAddEventCard } = useContext(StoryBoardContext);
+  return (
+    <Dialog
+      isOpen={eventDialogOpen}
+      className="event-dialog"
+      canOutsideClickClose
+    >
+      <div className="header">
+        <div className="event-title" contentEditable>
+          New Event
+        </div>
+        <Button
+          icon={'cross'}
+          minimal
+          onClick={() => openAddEventCard(false)}
+        />
+      </div>
+      <form action="post" className="body">
+        <Label>
+          Event Title:
+          <InputGroup className="event-title" placeholder="Event Title" />
+        </Label>
+        <Label>
+          Event Description:
+          <TextArea
+            className="event-desc"
+            placeholder="Event Description"
+            fill
+            id="event-dialog__event-desc"
+            growVertically={false}
+          />
+        </Label>
+      </form>
+    </Dialog>
+  );
 };
 
 export default EventDialog;
