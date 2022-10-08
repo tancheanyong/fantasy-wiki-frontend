0cf48cbe844933382f97b9897d97fe0a277815a6
tancheanyong <tancheanyong@gmail.com>
2022-10-08 09:40:00 +0800

    feat: Show btn on event node hover

--- src/components/storyBoard/StoryBoardCanvas/Nodes/EventNode.tsx
+++ src/components/storyBoard/StoryBoardCanvas/Nodes/EventNode.tsx
@@ -1,14 +1,25 @@
 import './EventNode.scss';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import { EventNodeData } from '../types';
+import { Button } from '@blueprintjs/core';
+import CustomBtn from '../commons/CustomBtn';
 
 type EventNodeProps = {
   data: EventNodeData;
 };
 
 const EventNode: FunctionComponent<EventNodeProps> = ({ data }) => {
+  const [showEditBtn, setShowEditBtn] = useState<boolean>(false);
+
   return (
-    <div className="event-node">
+    <div
+      className="event-node"
+      onMouseOver={() => setShowEditBtn(true)}
+      onMouseLeave={() => {
+        setShowEditBtn(false);
+      }}
+    >
+      <CustomBtn innerElement={'hi'} showBtn={showEditBtn} />
       <p className="header">{data.title}</p>
       <div className="characters"></div>
     </div>
